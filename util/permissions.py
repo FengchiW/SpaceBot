2c4d1c835e516c4b7d72741003a73004de524eb9
wilson <wilsonfwang@gmail.com>
2021-01-26 11:12:40 -0500

    oops

--- util/permissions.py
+++ util/permissions.py
@@ -9,20 +9,20 @@ def is_bot_owner():
 
 def is_staff():
     def predicate(ctx):
-        return any(role.name in ["Admins", "Trial Raid Leader", "Owner", "Moderators", "Halls Leader", "Head Leader",  "Oryx Leader", "Exalted Leader",  "Security", "Officer"] for role in ctx.author.roles)
+        return any(role.name in ["Admin", "Trial Raid Leader", "Owner", "Moderators", "Halls Leader", "Head Leader",  "Oryx Leader", "Exalted Leader",  "Security", "Officer"] for role in ctx.author.roles)
     return commands.check(predicate)
 
 def is_rl_or_higher():
     def predicate(ctx):
-        return any(role.name in ["Admins", "Halls ARL", "Oryx ARL", "Owner", "Moderators", "Halls Leader", "Head Leader",  "Oryx Leader", "Exalted Leader",  "Security", "Officer", "Event Organizer"] for role in ctx.author.roles)
+        return any(role.name in ["Admin", "Halls ARL", "Oryx ARL", "Owner", "Moderators", "Halls Leader", "Head Leader",  "Oryx Leader", "Exalted Leader",  "Security", "Officer", "Event Organizer"] for role in ctx.author.roles)
     return commands.check(predicate)
 
 def is_security():
     def predicate(ctx):
-        return any(role.name in ["Admins", "Owner", "Moderators", "Security", "Officer"] for role in ctx.author.roles)
+        return any(role.name in ["Admin", "Owner", "Moderators", "Security", "Officer"] for role in ctx.author.roles)
     return commands.check(predicate)
 
 def is_admin():
     def predicate(ctx):
-        return any(role.name in ["Admins", "Owner", "Bot Maker"] for role in ctx.author.roles)
+        return any(role.name in ["Admin", "Owner", "Bot Dev"] for role in ctx.author.roles)
     return commands.check(predicate)
